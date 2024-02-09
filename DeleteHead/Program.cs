c25df8fe357130cc405532df1fd2e76dfa651dc0
alaa <aamal79545@gmail.com>
2024-02-09 21:34:46 +0200

    Add project files.

---
+++ DeleteHead/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteHead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode DeleteHead(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            head = head.next;
+            return head;
+        }
+    }
+}
